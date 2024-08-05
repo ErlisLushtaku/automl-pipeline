07da5d75e6c7c14b39acac2582f42d4d7f3df707
Enes Hasani <enes.hasa1.1@hotmail.com>
2024-08-05 14:38:49 +0200

    Merged, and finished neps

--- run.py
+++ run.py
@@ -7,20 +7,13 @@ to this and you will need to output your predictions for the images of the test 
 to a file, which we will grade using github classrooms!
 """
 from __future__ import annotations
-from time import time
-
-from torch import nn, optim
 from pathlib import Path
-import numpy as np
-from automl.automl import AutoML
 from typer import Typer
 import logging
-from torchvision import transforms
 
-from automl.datasets import DataSets, GenericDataLoader
+from automl.datasets import DataSets
 from automl.model import Models
-from automl.trainer import LR_Scheduler, Optimizer, Trainer
-from automl.utils import calculate_mean_std
+from automl.neps import optimize_pipeline
 
 app = Typer()
 logger = logging.getLogger(__name__)
@@ -29,81 +22,16 @@ logger = logging.getLogger(__name__)
 def main(
     dataset: DataSets = DataSets.fashion.value,
     model: Models = Models.resnet18_1.value,
-    epochs: int = 1,
-    batch_size: int = 64,
     results_file: Path = Path("results/results.csv"),
     save_to: Path = Path("checkpoints/model.pt"),
 ):
 
-    start = time()
-    if dataset.factory.height > 128 or dataset.factory.width > 128:
-        transform = transforms.Compose(
-        [
-            transforms.Resize((128, 128)),
-            transforms.ToTensor(),
-            transforms.Normalize(*calculate_mean_std(dataset)),
-        ]
-    )
-    else:
-        transform = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Normalize(*calculate_mean_std(dataset)),
-            ]
-        )
-    print("Time to calculate mean and std:", time() - start)
-    augmentations = transforms.Compose(
-        [
-            transforms.AutoAugment(),
-        ]
-    )
-
-    dataloaders = GenericDataLoader(
-        dataset_name=dataset,
-        batch_size=batch_size,
-        num_workers=0,
-        transform=transform,
-        augmentations=augmentations,
-    )
-
-    kwargs = (
-        {
-        "img_height": dataset.factory.height,
-        "img_width": dataset.factory.width,
-        "channels": dataset.factory.channels,
-        }
-        if model == Models.cnn_model or model == Models.simple_cnn
-        else {}
-    )
-
-    model = model.factory(num_classes=dataset.factory.num_classes, **kwargs)
-    trainer = Trainer(
+    optimize_pipeline(
+        dataset=dataset,
         model=model,
         results_file=results_file,
-        #HP
-        optimizer=Optimizer.adamw,
-        loss_fn=nn.CrossEntropyLoss(),
-        lr_scheduler=LR_Scheduler.step,
-        lr=1e-3,
-        scheduler_gamma=0.97,
-        scheduler_step_size=200,
-        scheduler_step_every_epoch=False,
-        weight_decay=1e-2,
+        save_to=save_to,
     )
-    
-    trainer.train(
-        epochs=epochs,
-        train_loader=dataloaders.train_loader,
-        val_loader=dataloaders.val_loader,
-        save_best_to=save_to,
-        num_classes=dataset.factory.num_classes,
-    )
-
-    # Evaluate on test set
-    trainer.load_model(save_to)
-    results = trainer.eval(data_loader=dataloaders.test_loader, num_classes=dataset.factory.num_classes)
-    with open(str(results_file).replace(".csv", ".txt"), "w") as f:
-        f.write(str(results))
 
 if __name__ == "__main__":
     app()
--- src/automl/neps.py
+++ src/automl/neps.py
@@ -1,15 +1,67 @@
-
 import neps
 import numpy as np
+from torch import nn, optim
+from time import time
+from pathlib import Path
+import torch
+from torchvision import transforms
 from automl.automl import AutoML
+from automl.datasets import DataSets, GenericDataLoader
+from automl.model import Models
+from automl.trainer import LR_Scheduler, Optimizer, Trainer
+from automl.utils import calculate_mean_std
+
+def get_transformations(dataset):
+    start = time()
+    transform = transforms.Compose(
+        [
+            transforms.Resize((128, 128)),
+            transforms.ToTensor(),
+            transforms.Normalize(*calculate_mean_std(dataset)),
+        ]
+    ) if dataset.factory.height > 128 or dataset.factory.width > 128 else transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(*calculate_mean_std(dataset)),
+        ]
+    )
+    print(f"Transformations took {time() - start:.2f} seconds.")
+    return transform
+
+def get_augmentations():
+    return transforms.Compose(
+        [
+            transforms.AutoAugment(),
+        ]
+    )
+
+def get_data_loaders(dataset, batch_size, transform, augmentations):
+    return GenericDataLoader(
+        dataset_name=dataset,
+        batch_size=batch_size,
+        num_workers=0,
+        transform=transform,
+        augmentations=augmentations,
+    )
+
+def get_model(model, dataset):
+    kwargs = (
+        {
+        "img_height": dataset.factory.height,
+        "img_width": dataset.factory.width,
+        "channels": dataset.factory.channels,
+        }
+        if model == Models.cnn_model or model == Models.simple_cnn
+        else {}
+    )
+    return model.factory(num_classes=dataset.factory.num_classes, **kwargs)
 
 pipeline_space = {
     "lr": neps.FloatParameter(lower=1e-5, upper=1e-1, log=True),
     "batch_size": neps.IntegerParameter(lower=32, upper=128),
-    "epochs": neps.IntegerParameter(lower=5, upper=20),
+    "epochs": neps.IntegerParameter(lower=1, upper=2),
     "seed": neps.IntegerParameter(lower=0, upper=10000),
 }
-
 
 def objective_function(config, dataset_class):
     model = AutoML(
@@ -24,22 +76,58 @@ def objective_function(config, dataset_class):
     return {"accuracy": accuracy}
 
 
-def run_pipeline(lr, batch_size, epochs, seed, dataset_class):
-    config = {
-        "lr": lr,
-        "batch_size": batch_size,
-        "epochs": epochs,
-        "seed": seed,
-    }
+def run_pipeline(lr, batch_size, epochs, seed, dataset, model, results_file, save_to):
 
-    result = objective_function(config, dataset_class)
-    return result
+    trainer = Trainer(
+        model=get_model(model, dataset),
+        results_file=results_file,
+        optimizer=Optimizer.adamw,
+        loss_fn=nn.CrossEntropyLoss(),
+        lr_scheduler=LR_Scheduler.step,
+        lr=lr,
+        scheduler_gamma=0.97,
+        scheduler_step_size=200,
+        scheduler_step_every_epoch=False,
+        weight_decay=1e-2,
+        seed=seed,
+    )
+
+    dataloaders = get_data_loaders(
+        dataset,
+        batch_size,
+        get_transformations(dataset),
+        get_augmentations(),
+    )
+
+    _, _, validation_losses, _ =trainer.train(
+        epochs=epochs,
+        train_loader=dataloaders.train_loader,
+        val_loader=dataloaders.val_loader,
+        save_best_to=save_to,
+        num_classes=dataset.factory.num_classes,
+    )
+    
+    return dict(loss=validation_losses[-1])
 
 
-def optimize_pipeline(dataset_class):
+def optimize_pipeline(
+    dataset: DataSets = DataSets.fashion.value,
+    model: Models = Models.resnet18_1.value,
+    results_file: Path = Path("results/results.csv"),
+    save_to: Path = Path("checkpoints/model.pt"),
+):
 
     def wrapped_run_pipeline(lr, batch_size, epochs, seed):
-        return run_pipeline(lr, batch_size, epochs, seed, dataset_class)
+        return run_pipeline(
+            lr=lr,
+            batch_size=batch_size,
+            epochs=epochs,
+            seed=seed,
+            dataset=dataset,
+            model=model,
+            results_file=results_file,
+            save_to=save_to,
+        )
 
     neps_result = neps.run(
         run_pipeline=wrapped_run_pipeline,
